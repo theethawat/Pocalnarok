1ce36581009b5b1258ab2a1c48f212b26863ca63
Theethawat <5910110150@psu.ac.th>
2019-02-13 22:22:15 +0700

    Success Using Scanner from Text

--- src/Pocalnarok.java
+++ src/Pocalnarok.java
@@ -21,15 +21,34 @@ public class Pocalnarok {
     
     //Method
     public String inputSelecting(){
-        System.out.println(">>");
         String inputChoice = gameSelection.nextLine();
-        return inputChoice;
+        return inputChoice.trim();
     }
     
     public void startControl(){
         System.out.println("Selecting the thing that you want to do");
         System.out.println("A) Add Money  B) Buy Item  C) Kill  D) Viewinfo");
+        System.out.println(">>");
         //Selecting Activity
+        String inputValue = inputSelecting();
+        /*For Debug
+        System.out.println(inputValue);
+        */
+        if(inputValue.startsWith("A")){
+          System.out.println("You Select Add Money");
+        }
+        else if(inputValue.startsWith("B")){
+          System.out.println("You Select Buy Item");  
+        }
+        else if(inputValue.startsWith("C")){
+          System.out.println("You Select Kill");   
+        }
+        else if(inputValue.startsWith("D")){
+            System.out.println("You Select View Info"); 
+        }
+        else{
+            System.out.println("Please type only A, B , C , D");
+        }
     }
     
     public static void main(String args[]) {
