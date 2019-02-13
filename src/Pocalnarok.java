9cdc66e8ecb9b5e5618cbd8b78ba0be89fea0bec
Theethawat <5910110150@psu.ac.th>
2019-02-13 22:06:34 +0700

    Update Pocalnarok(Ragnarok Class) Add some method(or function) call

--- src/Pocalnarok.java
+++ src/Pocalnarok.java
@@ -7,16 +7,39 @@
  *
  * @author theet
  */
-import java.io.*;
 import java.util.*;
 public class Pocalnarok {
+    //Field
+    private Scanner gameSelection;
+    
+    
+    //Constructor    
+    public Pocalnarok(){
+        gameSelection = new Scanner (System.in);
+    }
+    
+    
+    //Method
+    public String inputSelecting(){
+        System.out.println(">>");
+        String inputChoice = gameSelection.nextLine();
+        return inputChoice;
+    }
+    
+    public void startControl(){
+        System.out.println("Selecting the thing that you want to do");
+        System.out.println("A) Add Money  B) Buy Item  C) Kill  D) Viewinfo");
+        //Selecting Activity
+    }
+    
     public static void main(String args[]) {
         System.out.println("Game Start");
+        //Create Novice
         Novice player = new Novice(100);
         int i=1;
         // Call Initail Value to start Games
         player.startGame(player.health,player.money);
-        System.out.println("Selecting the thing that you want to do");
-        System.out.println("A) Add Money  B) Buy Item  C) Kill  D) Viewinfo");
+        Pocalnarok gameControl = new Pocalnarok();
+        gameControl.startControl();
     }
 }
